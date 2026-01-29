8c4dd8fb8eed4466f0f7615edc68aad4536ff19d
Zeeshan-k250150 <k250150@nu.edu.pk>
2026-01-29 10:33:39 +0500

    Update 10.cpp

--- LAB 1/10.cpp
+++ LAB 1/10.cpp
@@ -1,17 +1,23 @@
-#include "iostream"
+#include <iostream>
 using namespace std;
-int main(){
+
+int main() {
     int arr[3][3];
-    cout<<"Enter Elements of the array\n";
-    for (int i = 0; i < 3; i++)
-    {
-        int sum=0;
-        for (int j = 0; j < 3; j++)
-        {
-            cin>>arr[i][j];
-            sum+=arr[i][j];
+
+    cout << "Enter elements of 3x3 matrix:\n";
+    for(int i = 0; i < 3; i++) {
+        for(int j = 0; j < 3; j++) {
+            cin >> arr[i][j];
         }
-        cout<<"The sum of row "<<i<<" is "<<sum<<endl;
     }
-    
+
+    for(int i = 0; i < 3; i++) {
+        int rowSum = 0;
+        for(int j = 0; j < 3; j++) {
+            rowSum += arr[i][j];
+        }
+        cout << "Sum of row " << i + 1 << ": " << rowSum << endl;
+    }
+
+    return 0;
 }
