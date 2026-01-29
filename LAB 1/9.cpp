c61077f8e9c892cdd14672e1cc8a797b809f36b4
Zeeshan-k250150 <k250150@nu.edu.pk>
2026-01-29 10:33:19 +0500

    Refactor array sum and reverse output
    Updated output messages for clarity and formatting.

--- LAB 1/9.cpp
+++ LAB 1/9.cpp
@@ -1,20 +1,20 @@
-#include "iostream"
+#include <iostream>
 using namespace std;
-int main(){
-    int arr[5];
-    int sum=0;
-    cout<<"Enter 5 elements"<<endl;
-    for (int i = 0; i < 5; i++)
-    {
-        cin>>arr[i];
-        sum+=arr[i];
+
+int main() {
+    int arr[5], sum = 0;
+
+    cout << "Enter 5 elements:\n";
+    for(int i = 0; i < 5; i++) {
+        cin >> arr[i];
+        sum += arr[i];
     }
-    
-    cout<<"The sum of the array is: "<<sum<<endl;
-    cout<<"The array reversed is: "<<endl;
-    for (int i = 4; i >= 0; i--)
-    {
-        cout<<arr[i]<<endl;
+
+    cout << "Sum: " << sum << endl;
+    cout << "Reverse order: ";
+    for(int i = 4; i >= 0; i--) {
+        cout << arr[i] << " ";
     }
-    
+
+    return 0;
 }
