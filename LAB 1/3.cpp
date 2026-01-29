2058923fdfef7ef68c47108a215099fc1c254028
Zeeshan-k250150 <k250150@nu.edu.pk>
2026-01-29 10:30:59 +0500

    Refactor input/output in main function
    Updated variable names and output formatting for clarity.

--- LAB 1/3.cpp
+++ LAB 1/3.cpp
@@ -1,25 +1,30 @@
-#include "iostream"
+#include <iostream>
 using namespace std;
-int main(){
-    int a;
-    float b;
-    double c;
-    char d;
-    bool e;
-    cout<<"Enter Integer Value: "<<endl;
-    cin>>a;
-    cout<<"Enter Float value : "<<endl;
-    cin>>b;
-    cout<<"Enter Double Value: "<<endl;
-    cin>>c;
-    cout<<"Enter Char Value: "<<endl;
-    cin>>d;
-    cout<<"Enter Bool Value: "<<endl;
-    cin>>e;
 
-    cout<<"Integer: "<<a<<endl;
-    cout<<"Float: "<<b<<endl;
-    cout<<"Double: "<<c<<endl;
-    cout<<"Char: "<<d<<endl;
-    cout<<"Bool: "<<e<<endl;
+int main() {
+    int i;
+    float f;
+    double d;
+    char c;
+    bool b;
+
+    cout << "Enter int: ";
+    cin >> i;
+    cout << "Enter float: ";
+    cin >> f;
+    cout << "Enter double: ";
+    cin >> d;
+    cout << "Enter char: ";
+    cin >> c;
+    cout << "Enter bool (0 or 1): ";
+    cin >> b;
+
+    cout << "\nValues entered:\n";
+    cout << "Int: " << i << endl;
+    cout << "Float: " << f << endl;
+    cout << "Double: " << d << endl;
+    cout << "Char: " << c << endl;
+    cout << "Bool: " << b << endl;
+
+    return 0;
 }
